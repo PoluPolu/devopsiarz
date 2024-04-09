4b8a49595bbd17ff791918358295079e7ff6966e
PoluPolu <ppproko@gmail.com>
2024-04-09 22:34:51 +0200

    2nd

--- main.go
+++ main.go
@@ -3,6 +3,27 @@ package main
 import "fmt"
 
 func main(){
+	pierw()
+	druga()
+}
 
+func druga() {
+	var x int32 = 1 
+	var y int32 = 1
+	
+	suma := x + y
+	fmt.Println("Suma wynosi:",suma)
+	fmt.Printf("suma jest typu %T\n",suma)
+
+	a := []string{"a","b","c","d","e"}
+	fmt.Printf("a jest typu %T\n",a)
+
+	fmt.Println(a)
+
+
+
+}
+
+func pierw() {
 	fmt.Println("Czesc Polu")
 }
